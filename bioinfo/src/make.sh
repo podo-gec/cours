7b9c127e221e8e16d4d9f7b20874a5ecaab7a492
chl <chl@aliquote.org>
2023-01-02 11:21:50 +0100

    [bioinfo] Update make rules
    Use a shell script instead of a Makefile to generate output files. Also
    separate slides from handouts, using a dedicated suffix.

---
+++ bioinfo/src/make.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+find . -name \*-s.org -print0 | xargs -0 org-beamer
+find . -name \*-s.org | xargs -I '{}' latexmk -c '{}'
+
+find . -type f -name \*-h.org | xargs -I '{}' org-babel -pdf '{}'
+find . -type f -name \*-h.org | xargs -I '{}' latexmk -c '{}'
+
+rm *.synctex.gz
+rm *.tex
+rm *.tex~
+rm *.bib
