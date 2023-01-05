9a2af46ec17eb03ff6b4f62b1fe06cde24fef300
chl <chl@aliquote.org>
2023-01-05 21:31:07 +0100

    [bioinfo] Update script for variant detection
    Trying out various settings for varscan in order to reproduce detection
    of SNP at position 3874633 (chrm 6, Pa_6_9890), which can be seen on
    Artemis. The polymorphism is followed by the following sequence:
    CTTCAAATGTAAACAG.

--- bioinfo/scripts/mutation.sh
+++ bioinfo/scripts/mutation.sh
@@ -6,31 +6,42 @@ cd "$WD" || exit
 mkdir mutation
 
 # Build the index for the reference genome and save aligned and unaligned reads
+echo -e "\033[0;32m[+] [bowtie2] Build index for reference genonme\033[0m"
 bowtie2-build Podospora_anserina_S_mat.fna mutation/Podospora_anserina_S_mat
+echo -e "\033[0;32m[+] [bowtie2] Compute aligned and unaligned reads\033[0m"
 bowtie2 -p 12 \
 	-x mutation/Podospora_anserina_S_mat \
 	-1 spoD1_R1_001.fastq.gz \
 	-2 spoD1_R2_001.fastq.gz \
 	--un-conc-gz mutation/unaligned \
 	--al-conc-gz mutation/aligned \
-	-S mutation/out.sam
+	-S mutation/out.sam >/dev/null 2>&1
 
 # Create BAM file
+echo -e "\033[0;32m[+] [samtools] Create BAM file, sort and index\033[0m"
 samtools view -b mutation/out.sam >mutation/tmp.bam
 samtools sort mutation/tmp.bam >mutation/out.bam
 rm mutation/tmp.bam
 samtools index mutation/out.bam mutation/out.bai
 
 # mpileup
+echo -e "\033[0;32m[+] [samtools] Create mpileup file\033[0m"
 samtools mpileup -B -f Podospora_anserina_S_mat.fna mutation/out.bam >mutation/out.pileup
 
 # varscan
 # NOTE: check read coverage to adjust default parameters below
-# not sure the output-snp and output-indel are really needed
-varscan pileup2cns mutation/out.pileup \
-	--min-coverage 20 \
-	--min-reads2 10 \
+echo -e "\033[0;32m[+] [varscan] Check for SNPs and indels\033[0m"
+
+varscan mpileup2snp mutation/out.pileup -v \
+	--min-coverage 40 \
+	--min-reads2 20 \
+	--min-var-freq 0.8 \
+	--min-avg-qual 15 \
+	>mutation/snp.varscan
+
+varscan mpileup2indel mutation/out.pileup -v \
+	--min-coverage 40 \
+	--min-reads2 20 \
 	--min-var-freq 0.9 \
-	--output-snp snp \
-	--output-indel indel \
-	>mutation/out.varscan
+	--min-avg-qual 15 \
+	>mutation/indel.varscan
