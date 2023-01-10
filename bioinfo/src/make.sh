c6d3a37127367920b0338a7f24c6ac3a9d6b1805
chl <chl@aliquote.org>
2023-01-10 21:34:23 +0100

    [bioinfo] Almost complete RNA-Seq tutorial

--- bioinfo/src/make.sh
+++ bioinfo/src/make.sh
@@ -3,8 +3,8 @@
 find . -name \*-s.org -print0 | xargs -0 org-beamer
 find . -name \*-s.org | xargs -I '{}' latexmk -c '{}'
 
-find . -type f -name \*-h.org | xargs -I '{}' org-babel -pdf '{}'
-find . -type f -name \*-h.org | xargs -I '{}' latexmk -c '{}'
+find . -name \*-h.org | xargs -I '{}' org-babel -pdf '{}'
+find . -name \*-h.org | xargs -I '{}' latexmk -c '{}'
 
 rm *.synctex.gz
 rm *.tex
