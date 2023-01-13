52c41ed3de66f32f1b63ba015a9a9a2e5a48925f
chl <chl@aliquote.org>
2023-01-13 21:07:14 +0100

    [bioinfo] Add data and update R script for RNA-Seq

--- bioinfo/scripts/ngs.r
+++ bioinfo/scripts/ngs.r
@@ -1,5 +1,3 @@
-## NOTE: This R script assumes that kallisto data are already available.
-##
 ## Kallisto will produce counts and TPM values in a TSV file (as well as
 ## an HDF5 file). Here is an example of Kallisto output:
 ##
@@ -8,15 +6,33 @@
 ## jgi|Podans1|100060|CE99695_1372 3146 3047 504 11.502
 ## jgi|Podans1|10015|CE9650_8706 1582 1483 1156 54.2042
 
-library("DESeq2")
-library("tximport")
+## NOTE: This R script assumes that kallisto data are already available.
+## Sample data are available in file kallisto.tar.gz.
+## Update path to reflect the correct location of Kallisto output on your HD
+WD <- "~/cwd/nobackup/galaxy/RNAseq/"
+setwd(WD)
+
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+}
+
+BiocManager::install(c("DESeq2", "tximport", "pheatmap", "BiocParallel"))
+
+library(DESeq2)
+library(tximport)
 library(pheatmap)
+
+library(BiocParallel)
+register(MulticoreParam(2))
 
 datadir <- dir(".", pattern = "out.\\.*")
 files <- file.path(datadir, "abundance.h5")
 names(files) <- gsub("out.", "", datadir)
 
-samples <- data.frame(run = names(files), condition = c("4h", "48h", " 24h"))
+samples <- data.frame(
+  run = names(files),
+  condition = c("soyb4", "corn4", "soyb4", "soyb4", "corn4", "corn4")
+)
 
 txi <- tximport(files, "kallisto", txOut = TRUE)
 dds <- DESeqDataSetFromTximport(txi, colData = samples, design = ~condition)
@@ -32,12 +48,40 @@ dd <- as.matrix(dist(t(assay(rld))))
 pheatmap(dd)
 plotPCA(rld, intgroup = c("condition"))
 
-## differential analysis
+## differential analysis (1)
 ddstab <- estimateDispersions(estimateSizeFactors(dds))
 r <- results(nbinomWaldTest(ddstab), pAdjustMethod = "BH")
 table(r$padj < 0.1)
-## r@metadata$filterThreshold
+# r@metadata$filterThreshold
 
 ## top genes
 plotMA(r)
 names(dds)[which(r$padj < 0.1)]
+
+## differential analysis (2)
+dds <- DESeq(dds)
+res <- results(dds)
+res
+
+summary(res)
+
+sum(res$padj < 0.1, na.rm = TRUE)
+
+plotMA(res, ylim = c(-10, 10))
+
+topGene <- rownames(res)[which.min(res$padj)]
+with(res[topGene, ], {
+  points(baseMean, log2FoldChange, col = "dodgerblue", cex = 2, lwd = 2)
+  text(baseMean, log2FoldChange, topGene, pos = 2, col = "dodgerblue")
+})
+
+plotCounts(dds, gene = which.min(res$padj), intgroup = "condition")
+
+dists <- dist(t(assay(ddstxi)))
+dists
+
+dd <- as.matrix(dists)
+pheatmap(dd,
+  clustering_distance_rows = dists,
+  clustering_distance_cols = dists
+)
